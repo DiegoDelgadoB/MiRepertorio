9aee746dbb49fb4d7dec0d4c1a2527e99fd2b683
Diego Delgado Barra <diego.delgadobarra@outlook.com>
2022-06-20 01:36:34 -0400

    LastCommit;Finished

--- main.js
+++ main.js
@@ -26,13 +26,60 @@ window.onload = () => {
     getData(tbody, cancion, artista, tono)
 };
 
-async function gerData(tbody, cancion, artista, tono) {
+async function getData(tbody, cancion, artista, tono) {
     await axios.get(url + "es").then((data) => {
         canciones = data.data;
         tbody.innerHTML = "";
         console.log(canciones)
         canciones.forEach((c, i) => {
             tbody.innerHTML += `
+      <tr>
+        <td>${i + 1}</td>
+        <td>${c.cancion}</td>
+        <td>${c.artista}</td>
+        <td>${c.tono}</td>
+        <td>
+          <button class="btn btn-warning" onclick="prepararCancion(${i},'${c.id
+                }')">Editar</button>
+          <button class="btn btn-danger" onclick="eliminarCancion(${i},'${c.id
+                }')">Eliminar</button>
+        </td>
+      </tr>
+    `;
+        });
+    });
+    cancion.value = "";
+    artista.value = "";
+    tono.value = "";
+};
+
+
+function eliminarCancion(i, id) {
+    axios.delete(url + "?id=" + id).then(() => {
+        alert("Canción " + canciones[i].cancion + " eliminada");
+        window.location.reload();
+    });
+};
+
+function prepararCancion(i, id) {
+    cancion.value = canciones[i].cancion;
+    artista.value = canciones[i].artista;
+    tono.value = canciones[i].tono;
+    document
+        .getElementById("editar")
+        .setAttribute("onclick", `editarCancion('${id}')`);
+    document.getElementById("agregar").style.display = "none";
+    document.getElementById("editar").style.display = "block";
+}
+function editarCancion(id) {
+    axios
+        .put(url, {
+            id,
+            cancion: cancion.value,
+            artista: artista.value,
+            tono: tono.value,
         })
-    })
-} 
+        .then(() => {
+            window.location.reload();
+        });
+};
